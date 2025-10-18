0417ff0e4a9b5352bcfddb5ec7ced2b3e32296a5
Kaedeeeeeeeeee <f.shera.09@gmail.com>
2025-10-18 17:55:11 +0900

    Initial Commit

---
+++ OCR_LLM/OCR_LLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCR_LLMApp.swift
+//  OCR_LLM
+//
+//  Created by USER on 2025/10/18.
+//
+
+import SwiftUI
+
+@main
+struct OCR_LLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OCR_LLMTests/OCR_LLMTests.swift
@@ -0,0 +1,17 @@
+//
+//  OCR_LLMTests.swift
+//  OCR_LLMTests
+//
+//  Created by USER on 2025/10/18.
+//
+
+import Testing
+@testable import OCR_LLM
+
+struct OCR_LLMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
